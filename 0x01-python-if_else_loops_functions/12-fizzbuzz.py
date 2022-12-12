75600da2860a6e51c05cee548ae34e58de122812
jesusenerio <jesusenerio50@gmail.com>
2022-12-12 04:37:36 -0800

    py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,15 +4,12 @@ def fizzbuzz():
     for i in range(1, 101):
         # when i modulo 3 or 5 is equal 0 print fizzbuzz
         if (i % 3) == 0 and (i % 5) == 0:
-            print("fizzbuzz", end=" ")
+            print("FizzBuzz", end=" ")
 # when i is 3 print fizz
         elif i == 3 or (i % 3) == 0:
-            print("fizz", end=" ")
+            print("Fizz", end=" ")
 # when i is 5 print buzz
         elif i == 5 or (i % 5) == 0:
-            print("buzz", end=" ")
-# when i modulo 3 or 5 is equal 0 print fizzbuzz
-        elif (i % 3) == 0 and (i % 5) == 0:
-            print("fizzbuzz", end=" ")
+            print("Buzz", end=" ")
         else:
             print(i, end=" ")
