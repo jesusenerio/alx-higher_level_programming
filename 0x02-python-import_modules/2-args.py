61f4539a9ec6d52863d1669d57912accba7445fa
jesusenerio <jesusenerio50@gmail.com>
2022-12-13 05:25:11 -0800

    py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-i = 1
-args = len(sys.argv) - 1
-if args == 0:
-    print("{} arguments.".format(args))
-elif args == 1:
-    print("{} argument:".format(args))
-else:
-    print("{} arguments:".format(args))
-while i <= args:
-    print("{}: {}".format(i, (sys.argv[i])))
-    i += 1
+if __name__ == "__main__":
+    import sys
+    i = 1
+    args = len(sys.argv) - 1
+    if args == 0:
+        print("{} arguments.".format(args))
+    elif args == 1:
+        print("{} argument:".format(args))
+    else:
+        print("{} arguments:".format(args))
+    while i <= args:
+        print("{}: {}".format(i, (sys.argv[i])))
+        i += 1
