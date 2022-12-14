23fc9575a35ebdf753105f661ebcc807eaccb28b
jesusenerio <jesusenerio50@gmail.com>
2022-12-14 11:55:14 -0800

    py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    num = my_list.copy()
+    if idx < 0 or idx > len(num):
+        return num
+    else:
+        num[idx] = element
+        return num
