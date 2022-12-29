997dfccaf1daaec1cdc599f3ed139249f018dc96
jesusenerio <jesusenerio50@gmail.com>
2022-12-29 09:14:33 -0800

    py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,12 @@
 
 
 class Square:
-    """initialize private class size"""
+    """Represent a square"""
 
     def __init__(self, size):
-        self._size = size
+        """ initialize a new square.
+
+        Args:
+            size(int): size of a new square
+        """
+        self.__size = size
