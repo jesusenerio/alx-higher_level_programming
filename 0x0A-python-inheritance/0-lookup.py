79dbcef0743dd4da9531136ac6bda77455f7666d
jesusenerio <jesusenerio50@gmail.com>
2023-01-11 07:45:44 -0800

    py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,21 @@
 def lookup(obj):
+    """
+    This function takes an object as an argument
+    and returns a list of all attributes and
+    methods of that object.
+    :param obj: any object
+    :return: list of attributes and methods of obj
+    """
     attributes = []
     methods = []
     for name in dir(obj):
+        """
+        Iterating through all attributes and methods of the object.
+        """
         item = getattr(obj, name)
+        """
+        Getting the value of the attribute or method with that name.
+        """
         if callable(item):
             methods.append(name)
         else:
