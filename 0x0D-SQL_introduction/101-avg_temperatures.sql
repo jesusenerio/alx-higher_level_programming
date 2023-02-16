11f2e2500d175046db43993f21719575689859f1
jesusenerio <jesusenerio50@gmail.com>
2023-02-16 07:14:31 -0800

    sql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- This Import in hbtn_0c_0 database this table download
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
