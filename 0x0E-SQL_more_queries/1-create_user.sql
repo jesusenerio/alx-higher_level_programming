eda6cc8a4d3bfc6f497bf0181e8ee7d26f04eeca
jesusenerio <jesusenerio50@gmail.com>
2023-02-18 06:25:19 -0800

    sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 --This create a user and gives it priviledges
-CREATE USER IF NOT EXIST user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT All PRIVILEGES ON * . * TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
