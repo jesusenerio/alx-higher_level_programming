d70b9c865f03ba05da5ec02b098315ca2df80099
jesusenerio <jesusenerio50@gmail.com>
2023-02-18 06:00:32 -0800

    sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- This list priviledges for mysql user
-SHOW GRANT for 'user_0d_1'@'localhost';
-SHOW GRANT for 'user_0d_2'@'localhost';
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
